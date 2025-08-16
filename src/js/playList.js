f898cf7d462b857e8f99dddb8e8c9ae2dbd863c3
hosein mohammadi <hosein011dev@gmail.com>
2025-08-17 01:54:47 +0330

    update

---
+++ src/js/playList.js
@@ -0,0 +1,141 @@
+let song = [
+    {
+        name: "Bato",
+        artist: "moein",
+        img: "batoMoein",
+        audio: "Moein - Ba To (128)"
+    },
+
+    {
+        name: "bito",
+        artist: "aref",
+        img: "bito",
+        audio: "Aref - Bi To 128"
+    }, 
+    
+    {
+        name: "Dopanjare",
+        artist: "Ebi & Googoosh",
+        img: "dopanjare",
+        audio: ""
+    }, 
+    
+    {
+        name: "Hamin Khobe",
+        artist: "Ebi",
+        img: "haminKhobe",
+        audio: ""
+    }, 
+    
+    {
+        name: "Lahzahaye Asheghane",
+        artist: "aref",
+        img: "lahzehayeAsheghane",
+        audio: ""
+    }, 
+    
+    {
+        name: "Bato",
+        artist: "Mahyar",
+        img: "batoMahyar",
+        audio: ""
+    }, 
+    
+    {
+        name: "HHRU",
+        artist: "Arta & Korosh",
+        img: "hhru",
+        audio: ""
+    }, 
+    
+    {
+        name: "Mowja",
+        artist: "Lara",
+        img: "mowja",
+        audio: ""
+    }, 
+    
+    {
+        name: "Yadam Mond",
+        artist: "Lara",
+        img: "yadamMondLara",
+        audio: ""
+    }, 
+    
+    {
+        name: "Moody",
+        artist: "Mahyar",
+        img: "moody",
+        audio: ""
+    }, 
+    
+    {
+        name: "Pishe To",
+        artist: "Shadmehr Aghili",
+        img: "pisheTo",
+        audio: ""
+    }, 
+    
+    {
+        name: "Bi Ehsas",
+        artist: "Shadmehr Aghili",
+        img: "biEhsas",
+        audio: ""
+    }, 
+    
+    {
+        name: "Dobare",
+        artist: "Googoosh & Leyla Forouhar & Sogand & Darya ...",
+        img: "dobare",
+        audio: ""
+    }, 
+    
+    {
+        name: "Pishkesh",
+        artist: "Googoosh",
+        img: "pishkesh",
+        audio: ""
+    }, 
+    
+    {
+        name: "Yadegary",
+        artist: "Siavash Ghomayshi",
+        img: "yadegary",
+        audio: ""
+    },
+    
+    {
+        name: "Yachi",
+        artist: "Masih & Arash AP",
+        img: "yaChi",
+        audio: ""
+    },
+    
+    {
+        name: "Bi Khabar",
+        artist: "",
+        img: "biKhabar",
+        audio: ""
+    },
+    
+    {
+        name: "Mosaken",
+        artist: "",
+        img: "mosaken",
+        audio: ""
+    },
+    
+    {
+        name: "Mastambaat",
+        artist: "",
+        img: "mastamBaat",
+        audio: ""
+    },
+    
+    {
+        name: "DonyaSorry",
+        artist: "",
+        img: "donyaSorry",
+        audio: ""
+    }
+]
