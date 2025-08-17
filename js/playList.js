d52bcb20f8c4d1a3357c3048f8c8adeb14759415
hosein mohammadi <hosein011dev@gmail.com>
2025-08-17 10:07:11 +0330

    update

---
+++ js/playList.js
@@ -0,0 +1,141 @@
+let songArray = [
+    {
+        name: "Bato",
+        artist: "moein",
+        img: "img/batoMoein.jpg",
+        audio: "Music/Moein - Ba To (128).mp3"
+    },
+
+    {
+        name: "bito",
+        artist: "aref",
+        img: "img/bito.jpg",
+        audio: "Music/Aref - Bi To 128.mp3"
+    }, 
+    
+    {
+        name: "Dopanjare",
+        artist: "Ebi & Googoosh",
+        img: "img/dopanjare.jpg",
+        audio: "Music/Googoosh And Ebi - Do Panjareh (128).mp3"
+    }, 
+    
+    {
+        name: "Hamin Khobe",
+        artist: "Ebi",
+        img: "img/haminKhobe.jpg",
+        audio: "Music/Ebi - Hamin KHoobe [128].mp3"
+    }, 
+    
+    {
+        name: "Lahzahaye Asheghane",
+        artist: "aref",
+        img: "img/lahzehayeAsheghane.jpg",
+        audio: "Music/Aref - Lahzehaye Asheghaneh (128).mp3"
+    }, 
+    
+    {
+        name: "Bato",
+        artist: "Mahyar",
+        img: "img/batoMahyar.jpg",
+        audio: "Music/Mahyar - Bato (320).mp3"
+    }, 
+    
+    {
+        name: "HHRU",
+        artist: "Arta & Korosh",
+        img: "img/hhru.jpg",
+        audio: "Music/Arta & Koorosh & Saaren - HHRU (128).mp3"
+    }, 
+    
+    {
+        name: "Mowja",
+        artist: "Lara",
+        img: "img/mowja.jpg",
+        audio: "Music/Lara - Mowja (128).mp3"
+    }, 
+    
+    {
+        name: "Yadam Mond",
+        artist: "Lara",
+        img: "img/yadamMondLara.jpg",
+        audio: "Music/Lara - Yadam Mond 128.mp3"
+    }, 
+    
+    {
+        name: "Moody",
+        artist: "Mahyar",
+        img: "img/moody.jpg",
+        audio: "Music/Mahyar - Moody (320).mp3"
+    }, 
+    
+    {
+        name: "Pishe To",
+        artist: "Shadmehr Aghili",
+        img: "img/pisheTo.jpg",
+        audio: "Music/Shadmehr Aghili - Pishe To (128).mp3"
+    }, 
+    
+    {
+        name: "Bi Ehsas",
+        artist: "Shadmehr Aghili",
+        img: "img/biEhsas.jpg",
+        audio: "Music/Shadmehr Aghili - Bi Ehsas.mp3"
+    }, 
+    
+    {
+        name: "Dobare",
+        artist: "Googoosh & Leyla Forouhar & Sogand & Darya ...",
+        img: "img/dobare.jpg",
+        audio: "Music/Unknown Artist - Iran Dobare Mishe Iran (320).mp3"
+    }, 
+    
+    {
+        name: "Pishkesh",
+        artist: "Googoosh",
+        img: "img/pishkesh.jpg",
+        audio: "Music/04 Pishkesh.mp3"
+    }, 
+    
+    {
+        name: "Yadegary",
+        artist: "Siavash Ghomayshi",
+        img: "img/yadegary.jpg",
+        audio: "Music/Siavash Ghomayshi-Alaki-128.mp3"
+    },
+    
+    {
+        name: "Yachi",
+        artist: "Masih & Arash AP",
+        img: "img/yaChi.jpg",
+        audio: "Music/Masih & Arash AP - Ya Chi (128).mp3"
+    },
+    
+    {
+        name: "Bi Khabar",
+        artist: "Reza Bahram",
+        img: "img/biKhabar.jpg",
+        audio: "Music/Reza Bahram - Bi Khabar (128).mp3"
+    },
+    
+    {
+        name: "Mosaken",
+        artist: "Novan",
+        img: "img/mosaken.jpg",
+        audio: "Music/Novan - Mosaken (128).mp3"
+    },
+    
+    {
+        name: "Mastambaat",
+        artist: "Erfan & Shahin Najafi & Morvarid",
+        img: "img/mastamBaat.jpg",
+        audio: "Music/Erfan & Shahin Najafi & Morvarid - Mastam Baat (128).mp3"
+    },
+    
+    {
+        name: "DonyaSorry",
+        artist: "Donya",
+        img: "img/donyaSorry.jpg",
+        audio: "Music/Donya - Sorry (128).mp3"
+    }
+]
